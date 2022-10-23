9b598681277daeb019c430f89f7bcc6ae17e08e4
JaykeBentes <jakebentes@hotmail.com>
2022-10-23 14:37:46 -0400

    Atualização projeto aula 3

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-
 function setCardType(type) {
     const colors = {
         "visa": ["#236D99", "#2D57F2"],
@@ -22,12 +21,23 @@ function setCardType(type) {
 globalThis.setCardType = setCardType
 
 
+//security code
 const securityCode = document.querySelector("#security-code")
 const securityCodePattern = {
     mask: "0000",
 }
 const securityCodeMasked = IMask(securityCode, securityCodePattern)
 
+securityCodeMasked.on("accept", () => {
+    updateSecurityCode(securityCodeMasked.value);
+})
+function updateSecurityCode(code) {
+    const ccSecurity = document.querySelector(".cc-security .value")
+    ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+
+//card date
 const expirationDate = document.querySelector("#expiration-date")
 const expirationDatePattern = {
     mask: "MM{/}YY",
@@ -46,6 +56,16 @@ const expirationDatePattern = {
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
 
+expirationDateMasked.on("accept", () => {
+    updateDate(expirationDateMasked.value);
+})
+function updateDate(date) {
+    const ccDate = document.querySelector(".cc-extra .value")
+    ccDate.innerText = date.length ===0 ? "02/32" : date
+}
+
+
+//card number
 const cardNumber = document.querySelector("#card-number")
 const cardNumberPattern = {
     mask: [
@@ -86,3 +106,34 @@ const cardNumberPattern = {
     },
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+cardNumberMasked.on("accept", () => {
+    const cardType = cardNumberMasked.masked.currentMask.cardType
+    setCardType(cardType)
+    updateCardNumber(cardNumberMasked.value);
+})
+function updateCardNumber(number) {
+    const ccCardNumber = document.querySelector(".cc-number")
+
+    ccCardNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+
+//card name
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+    const ccHolder = document.querySelector(".cc-holder .value")
+
+    ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+
+//button
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+    alert("Cartão adicionado!")
+})
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
